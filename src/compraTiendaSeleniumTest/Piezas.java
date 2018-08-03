0c3024176ac5496e3426b22c0c81ca74d1826476
cperezll <cperez2826gmail.com>
2018-08-03 09:55:32 +0200

    Continue with buy in www.oscaro.com

---
+++ src/compraTiendaSeleniumTest/Piezas.java
@@ -0,0 +1,22 @@
+package compraTiendaSeleniumTest;
+
+public class Piezas {
+
+	Integer coste;
+	String	nombrePieza;
+	String 	marca;
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("");
+	}
+
+	public Piezas() {
+		this.coste=0;
+		this.nombrePieza="null";
+		this.marca="null";
+	}
+	
+	
+}
